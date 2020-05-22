b2c2631850ffdad99fe40d1780dcc062dc05f718
João Pedro Vieira Belo <30899202+JoaoPedroBelo@users.noreply.github.com>
2020-05-22 12:01:54 +0100

    teste 1

---
+++ build/precache-manifest.6277e66a91604370dca345af2f4db131 3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ff2bb2ea9e2e9cf673230f198ecb59b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a28c92963da77104ae71",
+    "url": "/static/css/2.21aab339.chunk.css"
+  },
+  {
+    "revision": "dea7fd8062651f523476",
+    "url": "/static/css/main.cfaae4e1.chunk.css"
+  },
+  {
+    "revision": "a28c92963da77104ae71",
+    "url": "/static/js/2.4bdc1980.chunk.js"
+  },
+  {
+    "revision": "56820ebfc5f3088ac38bc6e129f4e973",
+    "url": "/static/js/2.4bdc1980.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dea7fd8062651f523476",
+    "url": "/static/js/main.69283465.chunk.js"
+  },
+  {
+    "revision": "16bf403f85a67a0fb936",
+    "url": "/static/js/runtime-main.12a6431d.js"
+  }
+]);
